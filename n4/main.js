2ab7e84b9cd58c4e86a8deb253e45ef6aa5f4e59
Anna <Saww.p10@gmail.com>
2019-10-01 10:59:51 +0300

    four

--- n4/main.js
+++ n4/main.js
@@ -1,1 +1,27 @@
-// Your code here
+let canv = document.createElement('canvas');
+let ctx = canv.getContext('2d');
+let h = canv.height = window.innerHeight;
+let w = canv.width = window.innerWidth;
+document.body.appendChild(canv);
+
+
+init();
+function draw(){
+	ctx.beginPath();
+	ctx.moveTo(w/2 - 100, h/2 - 100);
+	ctx.lineTo(w/2 + 100, h/2 - 100);
+	ctx.lineTo(w/2 + 100, h/2 + 100);
+	ctx.lineTo(w/2 - 100, h/2 + 100);
+	ctx.lineTo(w/2 - 100, h/2 - 100);
+	ctx.stroke();
+	ctx.closePath();
+}
+
+function init(){
+loop();
+}
+
+function loop(){
+	draw();
+requestAnimationFrame(loop);
+}
